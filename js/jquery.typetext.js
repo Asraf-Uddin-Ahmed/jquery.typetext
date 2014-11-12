65691805ed3a8ee1436a84bbf17986575dbc6e54
Asraf Uddin Ahmed <13ratul@gmail.com>
2014-11-12 20:30:19 +0600

    backspace activated.

--- js/jquery.typetext.js
+++ js/jquery.typetext.js
@@ -10,16 +10,28 @@
         var objToReturn = {};
 
         var defaultOptions = {
+            // COMMON
             message: "{ message: 'pass object like this' }",
+            baseText: "",
             index: 0,
             append: false,
             newLine: false,
             cursorChar: "|",
-            cursorShow: true,
+            cursorShow: false,
+
+            // TYPE
+            cursorShowAfterTextType: false,
             typeSpeed: 100,
             beforeTextType: function () { },
             afterTextType: function () { },
             onLetterType: function () { },
+
+            // BACKSPACE
+            cursorShowAfterTextBackspace: false,
+            backspaceSpeed: 100,
+            beforeTextBackspace: function () { },
+            afterTextBackspace: function () { },
+            onLetterBackspace: function () { },
         };
 
         var getOptions = function (customOptions) {
@@ -27,7 +39,15 @@
             if (typeof customOptions === 'object') {
                 $.extend(options, customOptions);
             }
+            // user modification of this cursorHTML, will be overriden
+            options.cursorObject = getCursorObject(options);
             return options;
+        }
+
+        var getCursorObject = function (options) {
+            // add CSS class named 'typeTextCursor' for customize the cursor
+            var cursorObject = $("<span class='typeTextCursor'>" + (options.cursorShow == true ? options.cursorChar : "") + "</span>");
+            return cursorObject;
         }
 
         objToReturn.getOptions = getOptions;
@@ -42,29 +62,13 @@
     var TypeText = function (target, userOptions) {
         var objToReturn = {};
 
+        /*
+        PRIVATE
+        */
         var targetObj = $(target);
         var options = OptionManager.getOptions(userOptions);
 
-
-        var showText = function () {
-            if (options.index >= options.message.length) {
-                options.afterTextType();
-                return targetObj;
-            }
-
-            options.onLetterType();
-
-            options.index++;
-            var cursor = (options.cursorShow == true ? options.cursorChar : "");
-            targetObj.text(options.message.substring(0, options.index) + cursor);
-
-            setTimeout(function () {
-                showText(targetObj, options);
-            }, options.typeSpeed);
-
-            return targetObj;
-        }
-
+        // WRITE
         var setNewLineAttribute = function () {
             if (options.newLine === true) {
                 targetObj.css("white-space", "pre");
@@ -76,14 +80,62 @@
 
         var setBaseTextOnMessage = function () {
             if (options.append === true) {
-                options.message = targetObj.text() + options.message;
-                options.index = targetObj.text().length;
+                options.baseText = targetObj.text();
             }
+            return options;
+        }
+
+        var showText = function () {
+            if (options.index >= options.message.length) {
+                options.afterTextType();
+                if (options.cursorShowAfterTextType === false) {
+                    options.cursorObject.remove();
+                }
+                return targetObj;
+            }
+
+            options.onLetterType();
+
+            options.index++;
+            targetObj.text(options.baseText + options.message.substring(0, options.index)).append(options.cursorObject);
+
+            setTimeout(function () {
+                showText(targetObj, options);
+            }, options.typeSpeed);
+
+            return targetObj;
+        }
+
+        // BACKSPACE
+        var prepareOptionForBackspace = function () {
+            options.message = targetObj.text();
+            options.index = options.message.length;
+            return options;
+        }
+
+        var backspaceText = function () {
+            if (options.index <= 0) {
+                options.afterTextBackspace();
+                if (options.cursorShowAfterTextBackspace === false) {
+                    options.cursorObject.remove();
+                }
+                return targetObj;
+            }
+
+            options.onLetterBackspace();
+
+            options.index--;
+            targetObj.text(options.baseText + options.message.substring(0, options.index)).append(options.cursorObject);
+
+            setTimeout(function () {
+                backspaceText(targetObj, options);
+            }, options.backspaceSpeed);
+
             return targetObj;
         }
 
 
-        var typeForward = function () {
+        var write = function () {
             options.beforeTextType();
             setNewLineAttribute();
             setBaseTextOnMessage();
@@ -91,7 +143,16 @@
             return targetObj;
         }
 
-        objToReturn.typeForward = typeForward;
+        var backspace = function () {
+            options.beforeTextBackspace();
+            prepareOptionForBackspace();
+            backspaceText();
+            return targetObj;
+        }
+
+
+        objToReturn.write = write;
+        objToReturn.backspace = backspace;
         return objToReturn;
     }
 
@@ -99,9 +160,9 @@
     
 
 
-    $.fn.typeText = function (userOptions) {
+    $.fn.typeText = function (command, userOptions) {
         var typeIt = new TypeText(this, userOptions);
-        return typeIt.typeForward();
+        return typeIt[command]();
     }
 
 
